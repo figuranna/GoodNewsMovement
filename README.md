99e38f669bd8e6efaf74db831e16412a550d84f0
Barbie Roberts <sertoanna03@gmail.com>
2023-09-14 11:48:28 +0200

    removed name input at newsletter.php

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Good News Movement Webpage 
-* Make buttons fucntional at index.php and newsletter.php
+A simple PHP application created for the **Good News Movement**.
+
+It uses no framework or CMS.
+## The Webpages
--- newsletter.php
+++ newsletter.php
@@ -6,12 +6,6 @@
         <h2>Stay updated about the good in the world</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ad beatae id laboriosam veritatis natus, amet fugiat cumque aperiam deserunt. Nobis voluptatibus aut laborum blanditiis explicabo cum deleniti! Reprehenderit, voluptatem.</p>
     </div>
-
-    <div class="newsletter-name">
-        <label for="inputName">Name: </label>
-        <input type="text" name="name" placeholder="Firstname" id="first-name" maxlength="50">
-        <input type="text" name="name" placeholder="Surname" id="surname" maxlength="50">
-    </div>
     <div class="newsletter-email">
         <label for="inputEmail">Email: </label>
         <input type="email" name="email" placeholder="joemama@example.com" id="inputEmail" maxlength="256">
